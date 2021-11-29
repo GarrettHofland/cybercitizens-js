c793c70e766823a17921d02608549eb7df1c496b
GarrettHofland <garretthofland@protonmail.ch>
2021-11-28 21:41:34 -0500

    Initial commit

---
+++ root/js/app.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+const menu = document.getElementsByClassName("navLinks");
+
+document.querySelector(".up-button").onclick = function(event) {
+    window.scrollTo(0, 0);
+}
+
+function exitMenuOnLinkClick() {
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+};
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+    links.forEach(link => {
+        link.addEventListener('click', exitMenuOnLinkClick);
+    });
+});
