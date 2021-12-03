291e37fa377d44cf126a3314e245e00bfbb6b656
GarrettHofland <garretthofland@protonmail.ch>
2021-12-03 16:55:45 -0500

    Basic Yoroi dapp connection setup

--- root/js/wallet-connector.js
+++ root/js/wallet-connector.js
@@ -1,5 +1,20 @@
 window.onload = function(){
-    document.getElementById("wallet").addEventListener("click",function(){
-        ergo_request_read_access();
+    document.getElementById("wallet").addEventListener("click", function(){
+        try {
+            ergo_request_read_access().then(access => {
+                if(access) {
+                    ergo.get_used_addresses().then(addresses => {
+                        addresses.forEach(addr => {
+                            document.getElementById('userWallet').innerText = "Address: " + addr;
+                        });
+                    });
+                }
+                else {
+                    document.getElementById('userWallet').innerText = "Error occured while connecting wallet, please ensure you have Yoroi Nightly and Yoroi DApp Connector Installed and try again.";
+                }
+            });
+        } catch (e) {
+            console.error(e);
+        }
     });
 }
