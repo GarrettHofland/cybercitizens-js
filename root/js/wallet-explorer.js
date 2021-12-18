326a8c5f941e75f018319e4e47ab229b5b9fdd0c
GarrettHofland <garretthofland@protonmail.ch>
2021-12-18 12:14:03 -0500

    Added metadata to the modals.

--- root/js/wallet-explorer.js
+++ root/js/wallet-explorer.js
@@ -297,6 +297,7 @@ const rarities = [{
 
 
 let mrPixel = "9iPtqBeeTMuAX4rkQqpHti2BKyd8ZXYRUuqynWJm3ShNpoSyowT";
+let randomTestAddr = "9fTuHJtqaaNYJAsTUJyBvM4xNVXGUGp8dYgHucKHHTDdbx24jLD";
 let auctionAddress = `5t19JGogcry9DRipPNcLs4mSnHYXQoqazPDMXXcdMixeH2mkgzMvWXjENsHRJzfHAFnTL5FBDHQCzBcnYg4CU1LcJZMmUXAaDcsKdgfBk4sE9BDbLt6Yxkjh6ow65HGCgxkwNAEArMAz8tqZL7GzKx4AvYVkqG3ExKggwDyVrvx7YzN8xeFtEUcnVkDKM8ow7YWW8eee2EidfYArPRd8fxQr5EuZVEiQbzKZ6m4xgtHfhsEptE3pNdt69F94gkytpounxBYpJPqfeZ8hVxLk8qaXTGFiJTDTt2p9D5ue4skZf4AGSLJyuzpMkjdifczQNc784ic1nbTAcjL3FKGHqnkaVwnCxU7go45X9ZFHwdpc6v67vFDoHzAAqypax4UFF1ux84X5G4xK5NFFjMZtvPyjqn2ErNXVgHBs2AkpngBPjnVRiN4sWkhR66NfBNpigU8PaTiB4Rim2FMZSXuyhRySCA1BV8ydVxz45T9VHqHA6WYkXp2ppAHmc29F8MrHX5Ew2x6amraFgvsdgAB3XiiEqEjRc83mhZVL1QgKi5CdeeGNYiXeCkxaRhG3j6r1JdAgzGDAQfN8sdRcEc1aYxbPfbqM1s81NFm7K1UmMUxrfCUp73poGAfV8FvQa2akyascKBaSCqvwuHW2ZP4oMoJHjZjTAgQjQF8cBNF9YLo6wXEtMQT5FYc3bHSgd4xZXCk2oHYjUSACW1Z5e7KZ3Qw1Sa2UvpMdWhbZ5Ncu99WT7v6nHFLJvHEPM7evr41nhCe9Yt3pAq4ee4rKCtEer4vQWq2b5UJSDXDj5VkVepQ5tmeXfXrBc42Yqucy6VeQSE7W66o4hQjwW1iN3yipmdTmpaAEASmbXwCxRSm7g4sNkfA969xo14PZQpBY3QUGqgCWoqJJVFWMhfvD53rzfgJpA4JH5B1fvY99q5iwbsAKdJfZi4fxub9QWZSNQfht4JqXMDmc6XTkWLE4VCxBRQYzF44H2E6mdf5EbZHUrpXj5c2VfC6PZGg9qmrz14aZjafM4M7kRTqMwVB8R9r7kXM1FWidGoprp2fRoJUALAKxKDSTVHX8ejT8zkSKJ5W45dSQjMe3WUDTeKhiy6Fqio2ukV8THaizTp6yZWxMVdu3a15pGBv1kmXZJEnLN9BsxyhnW2iGM7tvwK1jAneXeBH1uVdusR59j5ubCGKeoaS5ToC8Ky6wZ2iCyb2JF5CTvR4sMUg2ksmUm1dk8EoRjJ9i5gkqY`;
 let auctionAddresses = [auctionAddress];
 let auctions = [];
@@ -454,11 +455,13 @@ function buildPage() {
     let auctionCard = document.createElement('div');
     let assetName = document.createElement('h2');
     let assetImage = document.createElement('img');
-    let assetMetaData = auctions[i].description;
+    // let assetMetaData = auctions[i].description;
+    // let assetMetaData = document.createElement('p');
+    // let assetMetaData = JSON.parse(token.description[0].slice(1, token.description[0].length));
 
     assetImage.src = auctions[i].image;
     assetName.innerText = auctions[i].name;
-
+    // assetMetaData.innerText = auctions[i].description;
 
     auctionCard.classList.add("auction-card");
     auctionCard.classList.add("popupNFT" + i);
@@ -468,7 +471,7 @@ function buildPage() {
     container.append(auctionCard);
 
     document.querySelector(".popupNFT" + i).onclick = function () {
-      showNFTModal(auctions[i].image, auctions[i].name);
+      showNFTModal(auctions[i].image, auctions[i].name, auctions[i].description);
     }
   }
 if(nftEndIndex != auctions.length) {
