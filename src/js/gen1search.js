e662a5e4b5097897366987c50245190e015c0970
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-04-27 16:49:34 -0400

    search hidden until input

--- src/js/gen1search.js
+++ src/js/gen1search.js
@@ -5,13 +5,13 @@ function searchgen1() {
     filter = input.value.toUpperCase();
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName('li');
-  
+
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("a")[0];
       txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
+      if (filter!== "" && (txtValue.toUpperCase().indexOf(filter)) > -1) {
+        li[i].style.display = "block";
       } else {
         li[i].style.display = "none";
       }
