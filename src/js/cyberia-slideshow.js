e7eeb48f6410b989f98d99f13634fbc926ad2837
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-05-11 18:23:00 -0400

    added modal functionality
    after clicking mint instead of using second website

--- src/js/cyberia-slideshow.js
+++ src/js/cyberia-slideshow.js
@@ -80,65 +80,43 @@ const showpenthouse = () => {
   document.getElementById("fourth").style.display = "block";
 };
 
-const showgoal1 = () => {
-  document.getElementById("goal1").style.display = "block";
-  document.getElementById("itemframe1").style.display = "block";
-  document.getElementById("itemframe2").style.display = "none";
-  document.getElementById("goal2").style.display = "none";
+// Get the modal
+var modal = document.getElementById("modal-mint");
 
-  
+// Get the button that opens the modal
+var btn = document.getElementById("mint-estate");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-const showgoal2 = () => {
-  document.getElementById("goal2").style.display = "block";
-
-  document.getElementById("goal1").style.display = "none";
-  document.getElementById("itemframe1").style.display = "none";
-  document.getElementById("itemframe2").style.display = "block";
-  document.getElementById("goal3").style.display = "none";
-  document.getElementById("itemframe3").style.display = "none";
-  document.getElementById("goal4").style.display = "none";
-  document.getElementById("itemframe4").style.display = "none";
-  document.getElementById("goal5").style.display = "none";
-  document.getElementById("itemframe5").style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
-const showgoal3 = () => {
-  document.getElementById("itemframe2").style.display = "none";
-  document.getElementById("goal1").style.display = "none";
-  document.getElementById("itemframe1").style.display = "none";
-  document.getElementById("goal2").style.display = "none";
-  document.getElementById("goal3").style.display = "block";
-  document.getElementById("itemframe3").style.display = "block";
-  document.getElementById("goal4").style.display = "none";
-  document.getElementById("itemframe4").style.display = "none";
-  document.getElementById("goal5").style.display = "none";
-  document.getElementById("itemframe5").style.display = "none";
-  
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
-const showgoal4 = () => {
-  document.getElementById("itemframe2").style.display = "none";
-  document.getElementById("goal1").style.display = "none";
-  document.getElementById("itemframe1").style.display = "none";
-  document.getElementById("goal2").style.display = "none";
-  document.getElementById("goal3").style.display = "none";
-  document.getElementById("itemframe3").style.display = "none";
-  document.getElementById("goal4").style.display = "block";
-  document.getElementById("itemframe4").style.display = "block";
-  document.getElementById("goal5").style.display = "none";
-  document.getElementById("itemframe5").style.display = "none";
-  
-}
-const showgoal5 = () => {
-  document.getElementById("itemframe2").style.display = "none";
-  document.getElementById("goal1").style.display = "none";
-  document.getElementById("itemframe1").style.display = "none";
-  document.getElementById("goal2").style.display = "none";
-  document.getElementById("goal3").style.display = "none";
-  document.getElementById("itemframe3").style.display = "none";
-  document.getElementById("goal4").style.display = "none";
-  document.getElementById("itemframe4").style.display = "none";
-  document.getElementById("goal5").style.display = "block";
-  document.getElementById("itemframe5").style.display = "block";
+function copy() {
+  /* Get the text field */
+  var copyText = document.getElementById("mint-address");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
 }
